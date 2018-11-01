58414683f6cbdee95511d3c5bcc1c692fa2cabff
Michael Markl <marklmichael98@gmail.com>
2018-11-01 10:36:54 +0100

    WEBAPP-391: Fix flow resolve recognition

--- tools/build.js
+++ tools/build.js
@@ -3,9 +3,6 @@ const babel = require('@babel/core')
 
 const transpiled = babel.transformFileSync('./src/Headroom.js').code
 
-if (!fs.existsSync('./dist')) {
-  fs.mkdirSync('./dist')
-}
-fs.writeFileSync('./dist/index.js', transpiled)
+fs.writeFileSync('./index.js', transpiled)
 
-fs.copyFileSync('./src/Headroom.js', './dist/index.js.flow')
+fs.copyFileSync('./src/Headroom.js', './index.js.flow')
