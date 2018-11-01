84b32e37ad29a7632a2409b4d2e03de87819fed6
Michael Markl <marklmichael98@gmail.com>
2018-11-01 10:10:11 +0100

    WEBAPP-391: Fix build file

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 const babel = require('@babel/core')
 
-const transpiled = babel.transformFileSync('src/Headroom.js').code
-fs.writeFileSync('dist/index.js', transpiled)
+const transpiled = babel.transformFileSync('./src/Headroom.js').code
 
-fs.copyFileSync('src/Headroom.js', 'dist/index.js.flow')
+if (!fs.existsSync('./dist')) {
+  fs.mkdirSync('./dist')
+}
+fs.writeFileSync('./dist/index.js', transpiled)
+
+fs.copyFileSync('./src/Headroom.js', './dist/index.js.flow')
