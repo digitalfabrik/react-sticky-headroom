70c1c0c876f083680c2a13bcbbe70f05c0baadad
Michael Markl <marklmichael98@gmail.com>
2018-12-23 17:27:27 +0100

    HEADROOM-1: Remove unnecessary flow types

--- flow-typed/npm/escape-string-regexp_v1.x.x.js
+++
@@ -1,6 +0,0 @@
-// flow-typed signature: f84b42d9038e21762e6f8b6c02aec4e9
-// flow-typed version: 94e9f7e0a4/escape-string-regexp_v1.x.x/flow_>=v0.28.x
-
-declare module 'escape-string-regexp' {
-  declare module.exports: (input: string) => string;
-}
