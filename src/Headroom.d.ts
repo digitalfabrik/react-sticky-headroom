0337d04789043dd14d69a7d8f947ba25af7bea43
Michael Markl <marklmichael98@gmail.com>
2021-05-25 18:46:39 +0200

    HEADROOM-10: Add types for typescript

---
+++ src/Headroom.d.ts
@@ -0,0 +1,18 @@
+import * as React from 'react'
+
+declare type PropsType = {
+    children: Node,
+    scrollHeight: number,
+    pinStart: number,
+    height?: number,
+    onStickyTopChanged?: (number) => void,
+    positionStickyDisabled?: boolean,
+    parent?: HTMLElement,
+    zIndex?: number,
+    className?: string
+}
+
+declare class Headroom extends React.Component<PropsType> {
+}
+
+export default Headroom
